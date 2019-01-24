2b4fd954ae38df700b7bf977ac5a57edb7e92e49
joel burton <joeltburton@gmail.com>
2019-01-23 16:34:21 -0800

    added the file upload option on the front end, re-wrote the request body encoding map

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -7,73 +7,76 @@ export const fetchItemsStart = () => ({
 
 export const fetchedItems = resp => ({
   type: types.GET_ALL_ITEMS,
-  payload: resp
+  payload: resp,
 });
 
 export const fetchError = err => ({
   type: types.GET_ALL_ITEMS_ERR,
-  payload: err
+  payload: err,
 });
 
 export const searchValueChange = value => ({
   type: types.SEARCH_BOX_CHANGE,
-  payload: value
+  payload: value,
 });
 
-export const createItem = (item) => dispatch => {
-  console.log("button pushed");
-  console.log("item: ", item);
+export const createItem = item => (dispatch) => {
+  console.log('button pushed');
+  console.log('item: ', item);
 
 
-  const formBody = Object.keys(item).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(item[key])).join('&');
+  // const formBody = Object.keys(item).map(key => encodeURIComponent(key) + '=' + encodeURIComponent(item[key])).join('&');
 
+  const formBody = new FormData();
+  Object.keys(item).map(key => formBody.append(key, item[key]));
+
+  console.log('||||| made a formBody |||||');
   console.log(formBody);
   fetch('http://localhost:3001/addItem', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+      'Content-Type': 'multipart/form-data',
     },
-    body: formBody
+    body: formBody,
   }).then(response => response.json())
-    .then(data => {
+    .then((data) => {
       console.log('This item was stored in the database: ', data);
       console.log(dispatch(createdItem(data)));
       dispatch(fetchItemsStart());
-
     })
     .catch(() => dispatch(fetchError));
 };
 
-export const fetchItemsData = () => dispatch => {
+export const fetchItemsData = () => (dispatch) => {
   dispatch(fetchItemsStart());
 
   fetch('http://localhost:3001/allItems')
     .then(response => response.json())
-    .then(data => {
+    .then((data) => {
       console.log('we got the items');
       dispatch(fetchedItems(data));
     })
     .catch(() => dispatch(fetchError));
 };
 
-export const fetchSearchedItems = search => dispatch => {
+export const fetchSearchedItems = search => (dispatch) => {
   dispatch(fetchItemsStart());
 
   fetch(`http://localhost:3001/search/${search}`)
     .then(response => response.json())
-    .then(data => {
+    .then((data) => {
       console.log('we got the searched items');
       dispatch(fetchedItems(data));
     })
     .catch(() => dispatch(fetchError));
 };
 
-export const fetchCategoryItems = category => dispatch => {
+export const fetchCategoryItems = category => (dispatch) => {
   dispatch(fetchItemsStart());
 
   fetch(`http://localhost:3001/category/${category}`)
     .then(response => response.json())
-    .then(data => {
+    .then((data) => {
       console.log('we got the category items');
       dispatch(fetchedItems(data));
     })
--- client/components/ItemForm.jsx
+++ client/components/ItemForm.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 // import { Switch } from 'react-router-dom';
 import { Router } from 'react-router-dom';
-import { Switch, Route, withRouter, Link, browswerHistory, NavLink, Redirect, Prompt } from 'react-router-dom';
+import {
+  Switch, Route, withRouter, Link, browswerHistory, NavLink, Redirect, Prompt,
+} from 'react-router-dom';
 import { connect } from 'react-redux';
-import * as actions from '../actions/actions.js'
-const moment = require ('moment');
+import * as actions from '../actions/actions.js';
+
+const moment = require('moment');
 
 const mapStateToProps = store => ({
   items: store.items,
@@ -12,26 +15,29 @@ const mapStateToProps = store => ({
 
 const mapDispatchToProps = dispatch => ({
   createItem: ((itemInfo) => {
-    dispatch(actions.createItem(itemInfo))
-  })
+    dispatch(actions.createItem(itemInfo));
+  }),
 });
 
 class ItemForm extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       itemName: '',
       category: '',
       description: '',
       price: '',
       image: '',
+      selectedFile: null,
       fireRedirect: false,
-    }
+    };
     this.createItem = this.createItem.bind(this);
-    this.handleChange = this.handleChange.bind(this)
+    this.handleChange = this.handleChange.bind(this);
+    this.handleFileChange = this.handleFileChange.bind(this);
   }
+
   handleChange(e) {
-    console.log("e.target: ", e.target);
+    console.log('e.target: ', e.target);
 
     e.preventDefault();
     const newState = this.state;
@@ -39,58 +45,78 @@ class ItemForm extends Component {
     this.setState(newState);
   }
 
+  handleFileChange(e) {
+    const file = e.target.files[0];
+    this.setState({ selectedFile: file });
+  }
+
   createItem(e) {
     e.preventDefault();
-    let newItem = {
+    const newItem = {
       item_name: this.state.itemName,
       category: this.state.category,
       item_details: this.state.description,
       price: this.state.price,
       photo: this.state.image,
+      file: this.state.selectedFile,
       user_id: 1,
-      created_at: moment().format()
+      created_at: moment().format(),
     };
 
     this.props.createItem(newItem);
-    this.setState({ fireRedirect: true })
-
+    this.setState({ fireRedirect: true });
   }
 
   render() {
-    const { fireRedirect } = this.state
-    console.log("this.state: ", this.state);
+    const { fireRedirect } = this.state;
+    console.log('this.state: ', this.state);
     return (
       <div className="addItemForm">
         <h3>Add an item</h3>
-          Item Name<br></br>
-          <input className="item-name" id="itemName" onChange={this.handleChange} type="text"></input>
-          Category<br></br>
-          <select id='category' value={this.state.category} onChange={this.handleChange}>
-            <option value="entertainment">Entertainment</option>
-            <option value="household">Household</option>
-            <option value="outdoor">Outdoor</option>
-            <option value="toys">Toys</option>
-          </select>
-          Description<br></br>
-          <textarea className="item-description" id="description" onChange={this.handleChange}></textarea>
-          <br></br>
-          Price<br></br>
-          <input className="item-price" id="price" onChange={this.handleChange} type="number"></input>
-          Image Url<br></br>
-          <input className="item-image" id="image" onChange={this.handleChange} type="url"></input>
-          <button className="addItemButton" onClick={this.createItem}>Add Item</button> 
-          {fireRedirect && (<Redirect to={'/'}/>)}
+          Item Name
+        <br />
+        <input className="item-name" id="itemName" onChange={this.handleChange} type="text" />
+          Category
+        <br />
+        <select id="category" value={this.state.category} onChange={this.handleChange}>
+          <option value="entertainment">Entertainment</option>
+          <option value="household">Household</option>
+          <option value="outdoor">Outdoor</option>
+          <option value="toys">Toys</option>
+        </select>
+          Description
+        <br />
+        <textarea className="item-description" id="description" onChange={this.handleChange} />
+        <br />
+          Price
+        <br />
+        <input className="item-price" id="price" onChange={this.handleChange} type="number" />
+        <div>
+          <br />
+          <strong>Upload Photo</strong>
+          <br />
+            Photo URL:
+          <br />
+          <input className="item-image" id="image" onChange={this.handleChange} type="url" />
+            -OR-
+          <br />
+            Choose an Image
+          <br />
+          <input className="item-upload" id="file" onChange={this.handleFileChange} type="file" />
+        </div>
+        <br />
+        <div>
+          <button className="addItemButton" onClick={this.createItem}>Add Item</button>
+          {fireRedirect && (<Redirect to="/" />)}
+        </div>
       </div>
-    )
-
+    );
   }
-
 }
 
 
-
 // const ItemForm = (props) => {
-  
+
 //   return (
 //     <div id="modal-form">
 //       <h3>Add an item</h3>
@@ -105,7 +131,7 @@ class ItemForm extends Component {
 //           <option value="outdoor">Outdoor</option>
 //           <option value="toys">Toys</option>
 //         </select>
-       
+
 //         Description<br></br>
 //         <textarea></textarea>
 //         Price<br></br>
@@ -119,5 +145,5 @@ class ItemForm extends Component {
 
 // }
 
-//export default ItemForm;
+// export default ItemForm;
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ItemForm));
